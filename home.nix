1dbc55387024aba83a6d4d74b69587eab301c584
Peter Danulf <peterdanulf@MacBook-Pro-som-tillhor-Peter.local>
2024-06-29 11:01:15 +0200

    Remove simple from config

--- home.nix
+++ home.nix
@@ -72,7 +72,7 @@
         cd = "z";
         cat = "bat";
         find = "fd";
-        switch = "nix run nix-darwin -- switch --flake ~/.config/nix/.#simple";
+        switch = "nix run nix-darwin -- switch --flake ~/.config/nix";
         update = "(cd ~/.config/nix && nix flake update)";
       };
     };
