b740567a95d00a5ad964714c53f9790492bc557f
Peter Danulf <peter.danulf@gmail.com>
2025-11-08 08:08:32 +0100

    restructure

--- home.nix
+++ home.nix
@@ -6,26 +6,28 @@
   imports = [
     ./nushell.nix
   ];
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "peterdanulf";
-  home.homeDirectory = "/Users/peterdanulf";
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
+  home = {
+    # Home Manager needs a bit of information about you and the paths it should
+    # manage.
+    username = "peterdanulf";
+    homeDirectory = "/Users/peterdanulf";
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = [
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    stateVersion = "23.11"; # Please read the comment before changing.
+
+    # The home.packages option allows you to install Nix packages into your
+    # environment.
+    packages = [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
@@ -100,6 +102,53 @@
     pkgs.statix
   ];
 
+    # Home Manager is pretty good at managing dotfiles. The primary way to manage
+    # plain files is through 'home.file'.
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+    file = {
+      # gh repo clone peterdanulf/dotfiles ~/dotfiles
+      ".config/nvim" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/nvim";
+      };
+      ".config/ghostty/config" = {
+        text = ''
+          font-family = Operator Mono Lig
+          font-size = 16
+          macos-option-as-alt = right
+          command = ${pkgs.nushell}/bin/nu
+        '';
+      };
+    };
+
+    # Home Manager can also manage your environment variables through
+    # 'home.sessionVariables'. If you don't want to manage your shell through Home
+    # Manager then you have to manually source 'hm-session-vars.sh' located at
+    # either
+    #
+    #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+    #
+    # or
+    #
+    #  /etc/profiles/per-user/peterdanulf/etc/profile.d/hm-session-vars.sh
+    #
+    sessionVariables = {
+      EDITOR = "nvim";
+      TERM = "xterm-256color";
+      # To add token: echo "your-token-here" > ~/.claude_oauth_token
+      CLAUDE_CODE_OAUTH_TOKEN = ''$(cat ${config.home.homeDirectory}/.claude_oauth_token)'';
+      BWS_ACCESS_TOKEN = ''$(cat ${config.home.homeDirectory}/.bws_access_token)'';
+    };
+  };
+
   programs = {
     fzf = {
       enable = true;
@@ -148,52 +197,6 @@
         }
       '';
     };
-  };
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-  # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-  # # symlink to the Nix store copy.
-  # ".screenrc".source = dotfiles/screenrc;
-
-  # # You can also set the file content immediately.
-  # ".gradle/gradle.properties".text = ''
-  #   org.gradle.console=verbose
-  #   org.gradle.daemon.idletimeout=3600000
-  # '';
-  home.file = {
-    # gh repo clone peterdanulf/dotfiles ~/dotfiles
-    ".config/nvim" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/nvim";
-    };
-    ".config/ghostty/config" = {
-      text = ''
-        font-family = Operator Mono Lig
-        font-size = 16
-        macos-option-as-alt = right
-        command = ${pkgs.nushell}/bin/nu
-      '';
-    };
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/peterdanulf/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    EDITOR = "nvim";
-    TERM = "xterm-256color";
-    # To add token: echo "your-token-here" > ~/.claude_oauth_token
-    CLAUDE_CODE_OAUTH_TOKEN = ''$(cat ${config.home.homeDirectory}/.claude_oauth_token)'';
-    BWS_ACCESS_TOKEN = ''$(cat ${config.home.homeDirectory}/.bws_access_token)'';
   };
 
   # Let Home Manager install and manage itself.
